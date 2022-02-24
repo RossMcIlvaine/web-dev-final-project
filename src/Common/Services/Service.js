616fc632ef574e0fefb45c05dd745e3cdf425926
Margaret Farrell <mfarre22@nd.edu>
2022-02-24 16:01:31 -0500

    added parse, basic UI

---
+++ src/Common/Services/Service.js
@@ -0,0 +1,9 @@
+import Parse from "parse";
+
+export const getAllWorkouts = () => {
+    const Workout = Parse.Object.extend("Workout");
+    const query = new Parse.Query(Workout);
+    return query.find().then((results) => {
+    return results;
+  });
+};
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainModule = () => {
+  return (
+    <div>
+      This is the main module.
+      {/* Import data from service here to*/}
+    </div>
+  );
+};
+
+export default MainModule;
